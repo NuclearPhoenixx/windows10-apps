db0e1abfb378e06280458de1997b5133a6bca319
Phoenix1747 <apertured@public-files.de>
2020-01-11 15:18:13 +0100

    fix execution(policy) problems

--- windows10-apps uninstaller.bat
+++ windows10-apps uninstaller.bat
@@ -2,5 +2,5 @@
 TITLE Windows 10 Application Utility
 COLOR 0F
 
-powershell -executionpolicy remotesigned -Command "%~dp0main.ps1"
+powershell -executionpolicy bypass -Command "%~dp0main.ps1"
 exit
